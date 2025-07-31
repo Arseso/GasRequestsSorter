f1bcbf42511787120cb62eac4dc4afcdc08aa0c4
Artem Sokolov <arseso.main@gmail.com>
2025-07-31 22:08:23 +0300

    IMAP connection + login

---
+++ src/imap/IMAPClient.py
@@ -0,0 +1,42 @@
+import imaplib
+from src.resources import env
+
+from src.logging.logging import printToLog
+from src.models.logging.LogMessage import *
+
+logStringInfo = LogMessage(group=LogGroup.IMAP, type=LogType.INFO)
+logStringError = LogMessage(group=LogGroup.IMAP, type=LogType.ERROR)
+
+class IMAPClient:
+    
+    client: imaplib.IMAP4_SSL
+    
+    def __init__(self):
+        pass
+    
+    def connect(self):
+        try:
+            self.client = imaplib.IMAP4_SSL(
+                host= env.IMAP_SERVER,
+                port= env.IMAP_PORT
+            )
+            logStringInfo.message = f"Connected to {env.IMAP_SERVER}:{env.IMAP_PORT}"
+            printToLog(logStringInfo)
+        except:
+            logStringInfo.message = f"Error while connecting to server"
+            printToLog(logStringError)
+            
+    def login(self):
+        try:
+            self.client.login(env.IMAP_USER, env.IMAP_PWD)
+            logStringInfo.message = f"Login successfully"
+            printToLog(logStringInfo)
+        except:
+            logStringInfo.message = f"Error while login"
+            printToLog(logStringError)
+            
+            
+client = IMAPClient()
+client.connect()
+client.login()
+print(client.client.select("test"))
